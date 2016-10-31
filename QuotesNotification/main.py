cc91cd89adeb9ed080f2b3a4e7f562973574aaac
Rupesh Choudhary <ranjanrupesh1@gmail.com>
2016-10-31 20:32:19 +0530

    Final Touch of quotes Notification

--- QuotesNotification/main.py
+++ QuotesNotification/main.py
@@ -1,72 +1,24 @@
 __author__ = 'Rupesh Choudhary'
 
-from win32api import *
-from win32gui import *
-import win32con
-import sys, os
-import struct
-import time
 import random
 from time import sleep
+from WindowsBalloonTip import balloon_tip
 
-class WindowsBalloonTip:
-    def __init__(self, title, msg):
-        message_map = { win32con.WM_DESTROY: self.OnDestroy,}
-
-        # Register the window class.
-        wc = WNDCLASS()
-        hinst = wc.hInstance = GetModuleHandle(None)
-        wc.lpszClassName = 'PythonTaskbar'
-        wc.lpfnWndProc = message_map # could also specify a wndproc.
-        classAtom = RegisterClass(wc)
-
-        # Create the window.
-        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        self.hwnd = CreateWindow(classAtom, "Taskbar", style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, hinst, None)
-        UpdateWindow(self.hwnd)
-
-        # Icons managment
-        iconPathName = os.path.abspath(os.path.join( sys.path[0], 'balloontip.ico' ))
-        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-        try:
-            hicon = LoadImage(hinst, iconPathName, win32con.IMAGE_ICON, 0, 0, icon_flags)
-        except:
-            hicon = LoadIcon(0, win32con.IDI_APPLICATION)
-        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, 'Tooltip')
-
-        # Notify
-        Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20, hicon, 'Balloon Tooltip', msg, 200, title))
-        # self.show_balloon(title, msg)
-        time.sleep(5)
-
-        # Destroy
-        DestroyWindow(self.hwnd)
-        classAtom = UnregisterClass(classAtom, hinst)
-    def OnDestroy(self, hwnd, msg, wparam, lparam):
-        nid = (self.hwnd, 0)
-        Shell_NotifyIcon(NIM_DELETE, nid)
-        PostQuitMessage(0) # Terminate the app.
-
-# Function
-def balloon_tip(title, msg):
-    w=WindowsBalloonTip(msg, title)
-
-fw = open('quotes.txt','r')
+fw = open('quotes.txt', 'r')
 vocab = fw.read()
 vocab = vocab.split('\n')
+
 
 def sendmessage(title, message):
     balloon_tip(title, message)
     return
 
 while True:
-    i = random.randint(0,2002)
+    i = random.randint(0, 2002)
     if i % 2 == 0:
         if len(vocab[i]) < 118:
-            sendmessage(vocab[i],vocab[i+1])
-            sleep(10)
+            sendmessage(vocab[i], vocab[i + 1])
+            sleep(300)
         else:
             continue
     else:
